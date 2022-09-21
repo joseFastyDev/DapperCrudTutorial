ad7e1c317ae23b299126f981dab3d407bc7e724a
joseFastyDev <jose.rangel@fastydev.com>
2022-09-21 18:08:43 -0500

    Agregar archivos de proyecto.

---
+++ DapperCrudTutorial/Controllers/SuperHeroController.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace DapperCrudTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllSuperHeroes()
+        {
+            using var connection = new NpgsqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await SelectAllHeroes(connection);
+            return Ok(heroes);
+        }
+
+        [HttpGet("{heroId}")]
+        public async Task<ActionResult<List<SuperHero>>> GetHero(int heroId)
+        {
+            using var connection = new NpgsqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var hero = await connection.QueryAsync<SuperHero>("select * from \"superheroes\" where \"Id\" = @Id",
+                new { Id = heroId });
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateHero(SuperHero hero)
+        {
+            using var connection = new NpgsqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("insert into \"superheroes\"(" +
+                "name, firstname, lastname, place) values (@Name, @FirstName, @LastName, @Place)", hero);
+            return Ok(await SelectAllHeroes(connection));
+        }
+
+
+        private static async Task<IEnumerable<SuperHero>> SelectAllHeroes(NpgsqlConnection connection)
+        {
+            return await connection.QueryAsync<SuperHero>("select * from \"superheroes\"");
+        }
+    }
+}
---
+++ DapperCrudTutorial/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperCrudTutorial
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string FirstName { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string Place { get; set; } = String.Empty;
+    }
+}
